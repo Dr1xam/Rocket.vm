3277539df76b7b7402f8d07d46d88115311e25ce
Dr1xam <maksimdracenko280@gmail.com>
2025-11-19 15:30:33 +0200

    зміна шляхів

--- make_template.sh
+++ make_template.sh
@@ -2,7 +2,7 @@ source config
 
 echo "Відновлення VM $NEW_VM_ID зі сховища $TARGET_STORAGE..."
 # qmrestore розпаковує архів у віртуальну машину
-qmrestore "$FINAL_FILE" "$NEW_VM_ID" --storage "$TARGET_STORAGE" --unique
+qmrestore "$FINAL_FILE_PATH" "$NEW_VM_ID" --storage "$TARGET_STORAGE" --unique
 
 # Перевіряємо, чи успішно пройшло відновлення (код 0 = успіх)
 if [ $? -eq 0 ]; then
@@ -12,11 +12,8 @@ else
     exit 1
 fi
 
-echo "Перетворення VM $NEW_VM_ID на шаблон..."
-qm template "$NEW_VM_ID"
-
 # (Опційно) Видалити великий склеєний файл, щоб звільнити місце
-rm "$FINAL_FILE"
+rm "$FINAL_FILE_PATH"
 
 rm make_template.sh
 
