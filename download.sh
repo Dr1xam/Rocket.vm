79aa3563a91794023c539f375a636b57708ec629
Dr1xam <maksimdracenko280@gmail.com>
2025-11-20 12:12:48 +0200

    новий візуал і принцип завантаження убунти

--- download.sh
+++ download.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# Посилання на шматки
+# Посилання 
 URL_CONFIG="https://raw.githubusercontent.com/Dr1xam/deployment-tool/refs/heads/main/config"
-
 URL_INSTALL="https://raw.githubusercontent.com/Dr1xam/deployment-tool/refs/heads/main/install.sh"
-
 URL_MAKE_TEMPLATE="https://raw.githubusercontent.com/Dr1xam/deployment-tool/refs/heads/main/make_template.sh"
-
 URL_PARTS="https://github.com/Dr1xam/deployment-tool/releases/download/v1.0/"
-
 URL_DELETE_SCRIPT="https://raw.githubusercontent.com/Dr1xam/deployment-tool/refs/heads/refactor-core/delete-script.sh"
 
 # Шлях до фінального файлу бекапу
@@ -16,8 +12,18 @@ FINAL_FILE_NAME="vzdump-qemu-101.vma.zst"
 FINAL_FILE_DIRECTORY="/var/lib/vz/dump"
 FINAL_FILE_PATH="${FINAL_FILE_DIRECTORY}/${FINAL_FILE_NAME}"
 
+# Приблизний розмір архіву для коректної смужки (21 файл по 100мб + 1 шматок ~23мб)
+# Це потрібно, щоб pv показував саме відсотки (%). Якщо розмір зміниться, смужка просто дійде до кінця раніше або пізніше.
+TOTAL_SIZE="2120m"
+
 # початкова директорія 
 START_PATH=$PWD
+
+# --- ПЕРЕВІРКА ТА ВСТАНОВЛЕННЯ PV ---
+if ! command -v pv &> /dev/null; then
+    echo "Встановлення pv для відображення єдиної смужки завантаження..."
+    apt-get update -qq && apt-get install -y pv
+fi
 
 #Назви частин архіву з бекапом убунту сервера
 PART_PREFIX="part_archive_"
@@ -27,51 +33,31 @@ SUFFIXES=(
 
 cd ${FINAL_FILE_DIRECTORY}
 
+# Формуємо список усіх URL в один рядок
+URL_LIST=""
 for suffix in "${SUFFIXES[@]}"; do
-  # Формуємо повне ім'я файлу на сервері (наприклад, part_archive_aa)
-  PART_NAME="${PART_PREFIX}${suffix}"
-  
-  # Формуємо URL
-  URL="${URL_PARTS}${PART_NAME}"
-  
-  echo "Завантаження ${PART_NAME}..."
-
-  # Виконуємо завантаження
-  wget -q --show-progress -O "$PART_NAME" "$URL"
-  
-  # Перевірка, чи успішно скачався файл
-  if [ $? -ne 0 ]; then
-    echo "Помилка завантаження файлу ${LOCAL_NAME}. Перевірте інтернет або посилання."
-    rm ${PART_PREFIX}*
-    cd ${START_PATH}
-    rm download
-    exit 1
-  fi
+  URL_LIST="${URL_LIST} ${URL_PARTS}${PART_PREFIX}${suffix}"
 done
 
-echo "Усі частини завантажено успішно."
+echo "Початок завантаження архіву (єдиний потік)..."
 
-# --- СКЛЕЮВАННЯ ---
+# --- МАГІЯ ТУТ ---
+# 1. wget отримує список URL і качає їх по черзі в stdout (-O -)
+# 2. pv отримує дані, малює смужку (з розміром -s для відсотків)
+# 3. > пише все відразу у фінальний файл
+wget -q -O - $URL_LIST | pv -s $TOTAL_SIZE > "$FINAL_FILE_NAME"
 
-echo "Склеювання частин у файл: ${FINAL_FILE_NAME}..."
-
-# cat part_archive_a* склеїть їх у правильному алфавітному порядку (aa, ab, ac, ...)
-cat $PART_PREFIX* > $FINAL_FILE_NAME
-
-if [ $? -eq 0 ]; then
-  echo "Склеювання завершено. Файл ${FINAL_FILE_NAME} готовий."
-else
-  echo "Помилка під час склеювання файлів."
-  rm ${PART_PREFIX}*
-  cd ${START_PATH}
-  rm download
-  exit 1
+# Перевірка статусу (pipefail гарантує помилку, якщо wget впаде)
+if [ ${PIPESTATUS[0]} -ne 0 ]; then
+    echo "Помилка завантаження!"
+    rm "$FINAL_FILE_NAME"
+    cd ${START_PATH}
+    exit 1
 fi
 
-rm ${PART_PREFIX}*
+echo "Завантаження та склеювання завершено успішно."
 #скрипт який все удалить + конфіг
 wget -q --show-progress "$URL_CONFIG"
-
 wget -q --show-progress "$URL_DELETE_SCRIPT"
 #Перевірка чи завантажено скріпт 
 if [ ! -f delete-script.sh ] || [ ! -f config ]; then
@@ -84,7 +70,6 @@ fi
 
 #інсталяція інших файлів
 wget -q --show-progress "$URL_MAKE_TEMPLATE"
-
 #інсталтор в останю чергу
 wget -q --show-progress "$URL_INSTALL"
 
