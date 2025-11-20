07d7c5474b2338f18fea8312aca094d2da9ec490
Dr1xam <maksimdracenko280@gmail.com>
2025-11-20 14:25:25 +0200

    откат

--- make-template.sh
+++ make-template.sh
@@ -3,19 +3,6 @@ source config
 echo "Відновлення VM $NEW_VM_ID зі сховища $TARGET_STORAGE..."
 # qmrestore розпаковує архів у віртуальну машину
 qmrestore "$UBUNTU_BACKUP_TEMPLATE_NAME" "$NEW_VM_ID" --storage "$TARGET_STORAGE" --unique
-while IFS= read -r line; do
-    case "$line" in
-        progress*)
-            # \r повертає курсор на початок, \033[K очищає залишки тексту праворуч
-            echo -ne "\r$line\033[K"
-            ;;
-        *)
-            # Якщо це не прогрес (наприклад, помилка або фінальний статус),
-            # спочатку робимо новий рядок, щоб не затерти прогрес-бар
-            echo -e "\n$line"
-            ;;
-    esac
-done
 
 # Перевіряємо, чи успішно пройшло відновлення (код 0 = успіх)
 if [ $? -eq 0 ]; then
