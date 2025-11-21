fafb874686763f31b6dbef295182388ba17ed5d1
Dr1xam <maksimdracenko280@gmail.com>
2025-11-21 13:59:48 +0200

    я дурак замість змінної в конфіг текст писав

--- make-vm-settings.sh
+++ make-vm-settings.sh
@@ -9,7 +9,7 @@ cat > "$CONFIG_FILE" <<EOF
 #Місце розташування машин
 VM_TARGET_STORAGE="local-lvm"
 # Конфігурація для Шаблону 
-TEMPLATE_VM_ID=VM_ID
+TEMPLATE_VM_ID=${VM_ID}
 UBUNTU_BACKUP_TEMPLATE_NAME="vzdump-qemu-101.vma.zst"
 MAKE_TEMPLATE_LOG_FILE="make_template.log"
 EOF
