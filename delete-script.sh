42baae33a58d454b11a642eef2441bc93af41e2c
Dr1xam <maksimdracenko280@gmail.com>
2025-11-21 11:55:54 +0200

    Інтеграція скрипту створення конфігів в код

--- delete-script.sh
+++ delete-script.sh
@@ -1,8 +1,10 @@
-source install.conf
+source vm.conf
 rm -f make-template.sh
 rm -f install.sh
 rm -f ${UBUNTU_BACKUP_TEMPLATE_NAME}
-rm -f config
-
+rm -f install.conf
+rm -f make-vm-settings.sh
+rm -f vm.conf
 rm -f make_template.log
+
 rm -f delete-script.sh
