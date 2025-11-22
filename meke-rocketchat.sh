286c34e87958b9cb534ac4f492a53a3f6aea251b
Dr1xam <maksimdracenko280@gmail.com>
2025-11-22 10:19:21 +0200

    Код для створення машини

---
+++ meke-rocketchat.sh
@@ -0,0 +1,23 @@
+source vm.conf
+
+qm clone "$TEMPLATE_VM_ID" "$ROCKETCHAT_VM_ID" \
+  --name "$ROCKETCHAT_VM_HOSTNAME" \
+  --full 1 \
+  --storage "$VM_TARGET_STORAGE" > /dev/null
+
+if [ $? -ne 0 ]; then
+    echo " Критична помилка клонування! Перевірте, чи існує шаблон $TEMPLATE_VM_ID."
+    exit 1
+fi
+
+qm set "$ROCKETCHAT_VM_ID" \
+  --memory "$ROCKETCHAT_VM_RAM" \
+  --cores "$ROCKETCHAT_VM_CORES" \
+  --cpu cputype=host \
+  --net0 virtio,bridge="$ROCKETCHAT_VM_BRIDGE" \
+  --ipconfig0 ip="$ROCKETCHAT_VM_IP",gw="$GATEWAY" \
+  --nameserver "$ROCKETCHAT_VM_DNS" \
+  --onboot 1 > /dev/null
+
+qm resize "$ROCKETCHAT_VM_ID" scsi0 "$ROCKETCHAT_DISK" > /dev/null
+
