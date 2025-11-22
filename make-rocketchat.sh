e86b6aca23bfcc260850094d094bee3d5fa84bb7
Dr1xam <maksimdracenko280@gmail.com>
2025-11-22 10:49:59 +0200

    хай виводить усе

--- make-rocketchat.sh
+++ make-rocketchat.sh
@@ -3,7 +3,7 @@ source vm.conf
 qm clone "$TEMPLATE_VM_ID" "$ROCKETCHAT_VM_ID" \
   --name "$ROCKETCHAT_VM_HOSTNAME" \
   --full 1 \
-  --storage "$VM_TARGET_STORAGE" > /dev/null
+  --storage "$VM_TARGET_STORAGE" 
 
 if [ $? -ne 0 ]; then
     echo " Критична помилка клонування! Перевірте, чи існує шаблон $TEMPLATE_VM_ID."
@@ -17,7 +17,7 @@ qm set "$ROCKETCHAT_VM_ID" \
   --net0 virtio,bridge="$ROCKETCHAT_VM_BRIDGE" \
   --ipconfig0 ip="$ROCKETCHAT_VM_IP",gw="$GATEWAY" \
   --nameserver "$ROCKETCHAT_VM_DNS" \
-  --onboot 1 > /dev/null
+  --onboot 1 
 
-qm resize "$ROCKETCHAT_VM_ID" scsi0 "$ROCKETCHAT_DISK" > /dev/null
+qm resize "$ROCKETCHAT_VM_ID" scsi0 "$ROCKETCHAT_DISK" 
 
