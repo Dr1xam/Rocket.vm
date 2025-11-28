fefcf381fef4c517d76dc164eb8dcfb1cf42134b
Dr1xam <maksimdracenko280@gmail.com>
2025-11-28 11:59:27 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -39,7 +39,7 @@ done
 echo "Початок завантаження шаблону для віртуальних машин "
 
 # Вмикаємо зупинку при помилках
-set -e
+#set -e
 
 # Створюємо тимчасову папку для буфера
 mkdir -p "$TEMP_DIR"
@@ -72,7 +72,7 @@ echo "  out=src_code.tar.gz" >> "$ARIA_INPUT"
 # -x 4: по 4 потоки на файл
 # --summary-interval=1: оновлювати статус щосекунди (красивий вивід)
 
-if ! aria2c -i "$ARIA_INPUT" -d "$TEMP_DIR" -j 5 -x 4 -s 4 --summary-interval=1 ; then
+if ! aria2c -i "$ARIA_INPUT" -d "$TEMP_DIR" -j 5 -x 4 -s 4 --summary-interval=1 --console-log-level=warn; then
     echo "Помилка завантаження файлів!"
     rm -rf "$TEMP_DIR"
     rm -f "$FINAL_FILE_NAME"
