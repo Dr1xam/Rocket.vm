a0a8f1e3a4138d16e3941dfabe490826d983e451
Dr1xam <maksimdracenko280@gmail.com>
2025-12-02 10:32:14 +0200

    Update make-rocketchat.sh

--- src/make-rocketchat.sh
+++ src/make-rocketchat.sh
@@ -42,9 +42,10 @@ qm set "$ROCKETCHAT_VM_ID" \
   --cores "$ROCKETCHAT_VM_CORES" \
   --cpu cputype=host \
   --net0 virtio,bridge="$ROCKETCHAT_VM_BRIDGE" \
-  --ipconfig0 ip="$ROCKETCHAT_VM_IP",gw="$GATEWAY" \
+  --ipconfig0 ip="$ROCKETCHAT_VM_IP/CIDR",gw="$GATEWAY" \
   --nameserver "$ROCKETCHAT_VM_DNS" \
-  --onboot 1 >> "$DEPLOY_ROCKETCHAT_VM_LOG_FILE" 2>&1
+  --onboot 1 \
+  **--agent 1** >> "$DEPLOY_ROCKETCHAT_VM_LOG_FILE" 2>&1
 
 if [ $? -ne 0 ]; then
     echo "ПОМИЛКА"
